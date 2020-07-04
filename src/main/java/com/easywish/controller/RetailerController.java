12cca6fa202363c6c1620fb9dbe14176da1d64bc
Himani Agarwal <ahimani@thoughtworks.com>
2020-07-04 23:30:56 +0530

    [Himani] adds end point to get suppliers and retailers

--- src/main/java/com/easywish/controller/RetailerController.java
+++ src/main/java/com/easywish/controller/RetailerController.java
@@ -1,9 +1,18 @@
 package com.easywish;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/easywish/suppliers")
+@RequestMapping("/easywish/retailers")
 public class RetailerController {
+
+    @GetMapping
+    public ResponseEntity getRetailers() {
+        return ResponseEntity.ok(List.of("Name1", "Name2"));
+    }
+
 }
--- src/main/java/com/easywish/controller/SupplierController.java
+++ src/main/java/com/easywish/controller/SupplierController.java
@@ -1,9 +1,17 @@
 package com.easywish.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/easywish/suppliers")
 public class SupplierController {
+
+    @GetMapping
+    public ResponseEntity getSuppliers() {
+        return ResponseEntity.ok(List.of("Name1", "Name2"));
+    }
 }
